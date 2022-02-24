1b39dcd2afd54a057225198487c840e30569049a
Lukas-Batema <lukasbatema@gmail.com>
2022-02-24 15:21:21 -0500

    rip

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// hashcode 2022    NOTE ANDREW IS CRAP
+// hashcode 2022
 function randomIntFromInterval(min, max) { // generate random number between 1 and 10 
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -6,10 +6,10 @@ function randomIntFromInterval(min, max) { // generate random number between 1 a
 function main() {
   // This is where we will call all other functions and use general code not in other functions.
   for (let i = 0; i < 5; i++) {
-    for (let i = 0; i < 2; i++) {
+    for (let ii = 0; ii < 3; ii++) {
       var contributor = randomIntFromInterval(1, 3);
 
-      if (i === 0) {
+      if (ii === 0) {
         if (contributor === 1) {
           var contributorNameOne = 'Joe'; // biden
         } else if (contributor === 2) {
@@ -17,14 +17,8 @@ function main() {
         } else if (contributor === 3) {
           var contributorNameOne = 'Deez'; //nuts
         }
-      } else if (i === 1) {
-        if (contributorNameOne === 'Joe' && contributor === 1) {
-          i--;
-        } else if (contributorNameOne === 'Thanos' && contributor === 2) {
-          i--;
-        } else if (contributorNameOne === 'Deez' && contributor === 3) {
-          i--;
-        } else if (contributor === 1) {
+      } else if (ii === 1) {
+        if (contributor === 1) {
           var contributorNameTwo = 'Joe';
         } else if (contributor === 2) {
           var contributorNameTwo = 'Thanos';
@@ -33,19 +27,13 @@ function main() {
         } else {
           console.error('You know the drill, dummy!');
         }
-      } else if (i === 2) {
-        if (contributorNameOne === 'Joe' && contributor === 1) {
-          i--;
-        } else if (contributorNameOne === 'Thanos' && contributor === 2) {
-          i--;
-        } else if (contributorNameOne === 'Deez' && contributor === 3) {
-          i--;
-        } else if (contributor === 1) {
-          var contributorNameTwo = 'Joe';
-        } else if (contributor === 2) {
-          var contributorNameTwo = 'Thanos';
-        } else if (contributor === 3) {
-          var contributorNameTwo = 'Deez';
+      } else if (ii === 2) {
+        if (contributorNameOne === 'Joe' &&  contributorNameTwo === 'Thanos') {
+          var contributorNameThree = 'Deez';
+        } else if (contributorNameOne === 'Joe' &&  contributorNameTwo === 'Deez') {
+          var contributorNameThree = 'Thanos';
+        } else if (contributorNameOne === 'Thanos' &&  contributorNameTwo === 'Deez') {
+          var contributorNameThree = 'Joe';
         } else {
           console.error('How can you not name your kids!!!!!');
         }
@@ -57,8 +45,6 @@ function main() {
     var skillSet = [];
 
     var skillLevel;
-
-    var metor;
 
     if (skills === 1) {
       skillSet = ['HTML'];
@@ -128,5 +114,19 @@ function main() {
       console.error('Your programme sucks! Fix it you moron!'); // rude
     }
   }
+
+  var projects = [
+                  'Minecraft Dynamic Map',
+                  'Fortnite Sux Propaganda Site',
+                  'Beyblade Web Game',
+                  'Your Mom Simulator - Web Edition',
+                  'Amogus - Back to the Sus (Advanced Web Edition)'
+                ];
+
+  console.log(projects[0] + `: ${contributorNameOne}`);
+  console.log(projects[1] + `: ${contributorNameTwo}`);
+  console.log(projects[2] + `: ${contributorNameThree}`);
+  console.log(projects[3] + `: ${contributorNameTwo}`);
+  console.log(projects[4] + `: ${contributorNameOne}`);
 }
 main();
